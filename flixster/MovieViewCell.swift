1ca88470f93dc3a73264533a47ca4b08df8fa495
Kyle <chenk02@uscstudents.org>
2021-02-23 16:51:19 -0500

    finished

---
+++ flixster/MovieViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieViewCell.swift
+//  flixster
+//
+//  Created by Kyle Chen on 2/23/21.
+//
+
+import UIKit
+
+class MovieViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
